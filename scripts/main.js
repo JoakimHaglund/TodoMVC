860f7dcea088b6e7640d8043ff3874b1612cb383
JoakimHaglund <joakimhaglund@outlook.com>
2024-02-13 16:22:06 +0100

    @s1ggelin added remove button and we moved the create element to it's own function

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,35 @@
 
-        
+
+
+let form = document.querySelector('form');
+let noteContainer = document.querySelector('#notes-container');
+
+form.onsubmit = async event => {
+    event.preventDefault();
+
+    let input = form.elements.newNote.value;
+    let note = createNote(input);
+
+    noteContainer.appendChild(note);
+}
+function createNote(input) {
+
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    let note = document.createElement('p');
+    note.textContent = input;
+
+    let todos = document.createElement('li');
+
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.onclick = () => {
+        todos.remove();
+    }
     
-        let form = document.querySelector('form');
-        let noteContainer = document.querySelector('#notes-container');
-        
-        form.onsubmit = async event => {
-            event.preventDefault();
-
-            let checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            let note = document.createElement('p');
-            
-            let input = form.elements.newNote.value;
-            console.log(input);
-            note.textContent = input;
-
-            noteContainer.appendChild(checkbox);
-            noteContainer.appendChild(note);
-        }
+    todos.appendChild(checkbox);
+    todos.appendChild(note);
+    todos.appendChild(deleteButton);
+    
+    return todos;
+}
