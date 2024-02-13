8ccd75638effb8269453e8f38446c0404a2d5d32
JoakimHaglund <joakimhaglund@outlook.com>
2024-02-13 15:31:55 +0100

    Add element trough js, CSS template added
    Co-authored-by: Andreas Siggelin <andreassiggelin@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-import './noteElement.js';
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        let note = document.createElement('p');
+    
+        let form = document.querySelector('form');
+        let noteContainer = document.querySelector('#notes-container');
+        
+        form.onsubmit = async event => {
+            event.preventDefault();
+
+            let input = form.elements.newNote.value;
+            console.log(input);
+            note.textContent = input;
+
+            noteContainer.appendChild(checkbox);
+            noteContainer.appendChild(note);
+        }
