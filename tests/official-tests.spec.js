4ce4016fa95dc47cc5c54179c8a11c1717a1ba6c
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-02-16 11:24:20 +0100

    modified tests

--- tests/official-tests.spec.js
+++ tests/official-tests.spec.js
@@ -10,20 +10,29 @@ test('Add note and confirm it shows on the page', async ({ page }) => {
     await newNote.fill('Feed red pandas');
     await newNote.press('Enter');
 
-    await expect(page.getByRole('.active')).toHaveText('Feed red pandas');
+    await expect(page.locator('.active')).toHaveText('Feed red pandas');
 });
 //await expect(page.getByTestId('todo-title')).toHaveText()
 
 test('Add a note and confirm that the page shows "1 item left". Then, check the note and confirm that the page shows "0 items left', async ({ page }) => {
+    let newNote = page.getByPlaceholder('What needs to be done?');
+    await newNote.fill('Destroy the Death Star');
+    await newNote.press('Enter');
 
-
-
+    await expect(page)
   
 });
 
 test('Add 3 notes, check one of them, and confirm that the page shows "2 items left"', async ({ page }) => {
-   
+    let newNote = page.getByPlaceholder('What needs to be done?');
+    await newNote.fill('Feed red pandas');
+    await newNote.press('Enter'); 
   
+    await newNote.fill('Style Brad Pitt');
+    await newNote.press('Enter'); 
+  
+    await newNote.fill('Come up with a quiz for next lecture');
+    await newNote.press('Enter'); 
   
     
 });
