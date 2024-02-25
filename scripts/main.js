b1f2b631940d975c426afd830f6aae31923e85c0
JoakimHaglund <joakimhaglund@outlook.com>
2024-02-25 14:41:55 +0100

    Restructured js > html > css. Made tweeks to design and some bugfixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,168 +1,186 @@
 let form = document.querySelector('form');
 let noteContainer = document.querySelector('#notes-container');
 let arrowDown = document.querySelector('#complete-all');
-
 let itemsLeftCounter = document.querySelector('#items-left');
+let displayClass = 'all';
 
-
+// Event listener for the arrow-down button to complete all notes
 arrowDown.onclick = () => {
-    setNoteStatus();
-    //update count
-    updateItemsLeft();
+    SetNoteStatus();
+    UpdateView();
 };
 
+// Event listener for form submission to add a new note
 form.onsubmit = async event => {
     event.preventDefault();
-    form.insertAdjacentElement('afterbegin', arrowDown);
     let input = form.elements.newNote.value;
-    let note = createNote(input);
-    noteContainer.appendChild(note);
+    if (input) {
+        let note = CreateNote(input);
+        noteContainer.appendChild(note);
+    }
 
-    //show hidden elements
-    let footer = document.querySelector('.note-footer');
-    footer.classList.remove('hidden');
-    arrowDown.classList.remove('not-visible');
-
-
-    //update count
-    updateItemsLeft();
-
+    UpdateView();
     form.reset();
+};
+
+// Function to update the view
+function UpdateView() {
+    let notes = document.querySelectorAll('#notes-container > li')
+    let footer = document.querySelector('#note-footer');
+
+    // If notes exist, update the view
+    if (notes.length != 0) {
+        footer.classList.remove('hidden');
+        arrowDown.classList.remove('not-visible');
+        DisplayElementsWithClass(displayClass);
+        UpdateItemsLeftCounter();
+        FooterButtons();
+    }
+    // Hide elements if there are no notes
+    if (notes.length == 0) {
+        arrowDown.classList.add('not-visible');
+        footer.classList.add('hidden');
+    };
+    console.log('UpdateView')
 }
 
-function createNote(input) {
-
+// Function to create a new note element
+function CreateNote(input) {
+    // Create elements required for a new note
+    let note = document.createElement('li');
     let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+    let textElement = document.createElement('p');
+    let deleteButton = document.createElement('button');
 
+    // Setup element contents/classes
+    checkbox.type = 'checkbox';
+    textElement.textContent = input;
+    note.className = 'active';
+    deleteButton.textContent = '❌';
+
+    // Add buttons functionality
     checkbox.onchange = () => {
         if (checkbox.checked) {
             note.className = 'completed';
         }
         else {
             note.className = 'active';
-        }
-        //update count
-        updateItemsLeft();
-    }
-
-    let note = document.createElement('p');
-    note.textContent = input;
-    note.className = 'active';
-
-    let todos = document.createElement('li');
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = '❌';
+        };
+        UpdateView();
+    };
     deleteButton.onclick = () => {
-        todos.remove();
-        updateItemsLeft();
-    }
-        
+        note.remove();
+        UpdateView();
+    };
 
-    todos.appendChild(checkbox);
-    todos.appendChild(note);
-    todos.appendChild(deleteButton);
+    note.appendChild(checkbox);
+    note.appendChild(textElement);
+    note.appendChild(deleteButton);
+    return note;
+};
 
-    return todos;
-}
-function setNoteStatus() {
+// Function to toggle the status of notes
+function SetNoteStatus() {
     let notes = document.querySelectorAll('.active');
     let checkboxes = document.querySelectorAll('input[type=checkbox]');
     let setClass = 'completed';
 
+    // If no 'active' notes exist get all completed notes instead
     if (notes.length === 0) {
         notes = document.querySelectorAll('.completed')
         setClass = 'active';
-    }
+    };
 
     notes.forEach(note => {
         note.className = setClass;
 
     });
-    checkboxes.forEach(checkbox => {
 
+    checkboxes.forEach(checkbox => {
         if (setClass === 'completed') {
             checkbox.checked = true;
         }
         else {
             checkbox.checked = false;
-        }
+        };
     });
-}
-function updateItemsLeft() {
+};
+
+// Function to update the items left counter
+function UpdateItemsLeftCounter() {
     let itemsLeft = document.querySelectorAll('.active');
-
-    let showAllButton = document.querySelector('#show-all');
-    let showActiveButton = document.querySelector('#show-active');
-    let showCompletedButton = document.querySelector('#show-completed');
-    let clearCompletedButton = document.querySelector('#clear-completed');
-
-    showAllButton.onclick = () => {
-        HideElementsWithClass('all');
-        updateItemsLeft();
-    };
-    showActiveButton.onclick = () => {
-        HideElementsWithClass('active');
-        updateItemsLeft();
-    };
-    showCompletedButton.onclick = () => {
-        HideElementsWithClass('completed');
-        updateItemsLeft();
-    };
-    if(document.querySelectorAll('#notes-container .completed').length > 0){
-        clearCompletedButton.classList.remove('not-visible');
-    }
-    else{
-        clearCompletedButton.classList.add('not-visible');
-    }
-
-    //Hide elements if there are no notes
-    let notes = document.querySelectorAll('#notes-container > li')
-    if (notes.length === 0) {
-        let footer = document.querySelector('.note-footer');
-        footer.classList.add('hidden');
-        arrowDown.classList.add('not-visible');
-        clearCompletedButton.classList.add('not-visible');
-    }
-
-
-    clearCompletedButton.onclick = () => {
-        let completedNotes = document.querySelectorAll('.completed');
-    
-        completedNotes.forEach(note => {
-            note.closest('li').remove();
-        });
-    
-        updateItemsLeft();
-    };
-
     if (itemsLeft.length === 1) {
         itemsLeftCounter.textContent = itemsLeft.length + ' item left';
     }
     else {
         itemsLeftCounter.textContent = itemsLeft.length + ' items left';
-    }
+    };
 };
 
-/*
-get list of li with class 
-if class is what we don't want 	-> append hidden class
-if class is what we  want 		-> remove hidden class
-*/
-function HideElementsWithClass(className) {
-    let elements = document.querySelectorAll('#notes-container p');
+// Function to highlight the active footer button
+function SetFooterHighlight(highlightButton) {
+    filterButtons = document.querySelectorAll('#filters button');
+    filterButtons.forEach(button => {
+        button.classList.remove('highlight');
+    });
+    highlightButton.classList.add('highlight');
+}
+
+// Function to handle footer buttons
+function FooterButtons() {
+    let showAllButton = document.querySelector('#show-all');
+    let showActiveButton = document.querySelector('#show-active');
+    let showCompletedButton = document.querySelector('#show-completed');
+    let clearCompletedButton = document.querySelector('#clear-completed');
+
+    // Event listeners for footer buttons
+    showAllButton.onclick = () => {
+        displayClass = 'all';
+        DisplayElementsWithClass(displayClass);
+        SetFooterHighlight(showAllButton)
+        UpdateView();
+    };
+    showActiveButton.onclick = () => {
+        displayClass = 'active';
+        DisplayElementsWithClass(displayClass);
+        SetFooterHighlight(showActiveButton)
+        UpdateView();
+    };
+    showCompletedButton.onclick = () => {
+        displayClass = 'completed';
+        DisplayElementsWithClass(displayClass);
+        SetFooterHighlight(showCompletedButton)
+        UpdateView();
+    };
+    clearCompletedButton.onclick = () => {
+        let completedNotes = document.querySelectorAll('.completed');
+        completedNotes.forEach(note => {
+            note.closest('li').remove();
+        });
+        UpdateView();
+    };
+
+    // Toggle visibility of clear completed button based on completed notes
+    if (document.querySelectorAll('#notes-container .completed').length > 0) {
+        clearCompletedButton.classList.remove('not-visible');
+    }
+    else {
+        clearCompletedButton.classList.add('not-visible');
+    };
+};
+
+// Function to display elements with a specific classname and hide others
+function DisplayElementsWithClass(className) {
+    let elements = document.querySelectorAll('#notes-container li');
 
     elements.forEach(element => {
-        if (element.classList.contains(className)) {
-            element.closest('li').classList.remove('hidden');
-        }
-        else if (className === 'all') {
-            element.closest('li').classList.remove('hidden');
+        if (element.classList.contains(className) || className === 'all') {
+            element.classList.remove('hidden');
         }
         else {
-            element.closest('li').classList.add('hidden');
-        }
+            element.classList.add('hidden');
+        };
 
     });
-}
+};
 
--- tests/official-tests.spec.js
+++ tests/official-tests.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5500/index.html');
-  });
+});
 
 
 test('Add note and confirm it shows on the page', async ({ page }) => {
@@ -19,21 +19,20 @@ test('Add a note and confirm that the page shows "1 item left". Then, check the 
     await newNote.press('Enter');
 
     await expect(page.locator('#items-left')).toHaveText('1 item left');
-  
+
 });
 
 test('Add 3 notes, check one of them, and confirm that the page shows "2 items left"', async ({ page }) => {
     let newNote = page.getByPlaceholder('What needs to be done?');
     await newNote.fill('Feed red pandas');
-    await newNote.press('Enter'); 
+    await newNote.press('Enter');
     await newNote.fill('Style Brad Pitt');
-    await newNote.press('Enter'); 
+    await newNote.press('Enter');
     await newNote.fill('Come up with a quiz for next lecture');
-    await newNote.press('Enter'); 
+    await newNote.press('Enter');
     let checkbox = await page.locator('#notes-container > li:nth-child(1) > input');
     await checkbox.click();
 
     await expect(page.locator('#items-left')).toHaveText('2 items left');
-    
+
 });
-  
